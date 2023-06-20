3deee20b6794c0db62ade2fc7bb92414bbe5090f
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2023-06-20 17:34:06 +0200

    Use city data instead of generating it, calculate initially adjacency matrix

--- sequential.cpp
+++ sequential.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 #include <algorithm>
 #include <random>
-#include <thread>
 #include <mutex>
+#include <fstream>
+#include <sstream>
+
 
 // Constants
-const int NUM_CITIES = 10;
-const int POPULATION_SIZE = 100;
-const int NUM_ITERATIONS = 20;
-const int MAX_COORDINATE = 1000;
+const int POPULATION_SIZE = 1000;
+const int NUM_ITERATIONS = 100;
 const float MUTATION_RATE = 0.2;
 const float ELITISM_RATE = 0.2;
 
 // City struct
 struct City {
     int id;
-    int x;
-    int y;
+    double x;
+    double y;
 };
 
 // Chromosome struct
@@ -31,7 +31,7 @@ struct Chromosome {
 // Global variables
 std::vector<City> cities;
 std::vector<Chromosome> oldPopulation;
-std::vector<Chromosome> nextPopulation;    
+std::vector<Chromosome> nextPopulation;
 
 // Function to calculate the distance between two cities
 double calculateDistance(const City& city1, const City& city2) {
@@ -40,38 +40,56 @@ double calculateDistance(const City& city1, const City& city2) {
     return std::sqrt(dx * dx + dy * dy);
 }
 
+// Function to generate the adjacency matrix of distances between cities
+std::vector<std::vector<double>> generateAdjacencyMatrix(const std::vector<City>& cities) {
+    std::vector<std::vector<double>> adjacencyMatrix(cities.size(), std::vector<double>(cities.size(), 0.0));
+    for (int i = 0; i < cities.size(); ++i) {
+        for (int j = 0; j < cities.size(); ++j) {
+            adjacencyMatrix[i][j] = calculateDistance(cities[i], cities[j]);
+        }
+    }
+    return adjacencyMatrix;
+}
+
+// Function to get distance from the adjacency matrix
+double getDistance(const std::vector<std::vector<double>>& adjacencyMatrix, int cityIndex1, int cityIndex2) {
+    return adjacencyMatrix[cityIndex1][cityIndex2];
+}
+
 // Function to calculate the fitness of a chromosome
-double calculateFitness(const Chromosome& chromosome) {
+double calculateFitness(const Chromosome& chromosome, const std::vector<std::vector<double>>& adjacencyMatrix) {
     double totalDistance = 0.0;
-    for (int i = 0; i < NUM_CITIES - 1; ++i) {
+    for (int i = 0; i < cities.size() - 1; ++i) {
         int cityIndex1 = chromosome.path[i];
         int cityIndex2 = chromosome.path[i + 1];
-        totalDistance += calculateDistance(cities[cityIndex1], cities[cityIndex2]);
+        totalDistance += getDistance(adjacencyMatrix, cityIndex1, cityIndex2);
     }
     // Add the distance from the last city back to the first city
-    totalDistance += calculateDistance(cities[chromosome.path[NUM_CITIES - 1]], cities[chromosome.path[0]]);
+    totalDistance += getDistance(adjacencyMatrix, chromosome.path[cities.size() - 1], chromosome.path[0]);
     return 1.0 / totalDistance;
 }
 
+
 // Function to generate a random chromosome
-Chromosome generateRandomChromosome() {
+Chromosome generateRandomChromosome(const std::vector<std::vector<double>>& adjacencyMatrix) {
     Chromosome chromosome;
-    chromosome.path.resize(NUM_CITIES);
-    for (int i = 0; i < NUM_CITIES; ++i) {
+    chromosome.path.resize(cities.size());
+    for (int i = 0; i < cities.size(); ++i) {
         chromosome.path[i] = i;
     }
     std::random_shuffle(chromosome.path.begin() + 1, chromosome.path.end());
-    chromosome.fitness = calculateFitness(chromosome);
+    chromosome.fitness = calculateFitness(chromosome, adjacencyMatrix);
     return chromosome;
 }
 
-// Function to perform crossover between two parent chromosomes
+
+// Function to perform crossover between two parent chromosomes. It uses "Ordered Crossover (OX)"
 void crossover(Chromosome& child, const Chromosome& parent1, const Chromosome& parent2) {
-    std::vector<bool> visited(NUM_CITIES, false);
+    std::vector<bool> visited(cities.size(), false);
 
     // Select a random subset of the parent 1's path
-    int randA = std::rand() % NUM_CITIES;
-    int randB = std::rand() % NUM_CITIES;
+    int randA = std::rand() % cities.size();
+    int randB = std::rand() % cities.size();
     int startPos = std::min(randA, randB);
     int endPos = std::max(randA, randB);
 
@@ -81,9 +99,9 @@ void crossover(Chromosome& child, const Chromosome& parent1, const Chromosome& p
         visited[child.path[i]] = true;
     }
 
-    // Fill the rest of the child's path with the remaining cities from parent 2
+    // Fill the rest of the child's path with the remaining cities from parent 2, in order left to right
     int parent2Pos = 0;
-    for (int i = 0; i < NUM_CITIES; ++i) {
+    for (int i = 0; i < cities.size(); ++i) {
         if (parent2Pos == startPos)
             parent2Pos = endPos + 1;
 
@@ -96,19 +114,19 @@ void crossover(Chromosome& child, const Chromosome& parent1, const Chromosome& p
 }
 
 
-// Mutate function: up to MUTATION_RATE cities are swapped
+// Mutate function: up to MUTATION_RATE*NUM_CITIES cities are swapped
 void mutate(Chromosome& chromosome) {
-   int numMutations = std::rand() % (int)(MUTATION_RATE * NUM_CITIES);
+   int numMutations = std::rand() % (int)(MUTATION_RATE * cities.size());
     for (int i = 0; i < numMutations; ++i) {
-         int index1 = std::rand() % NUM_CITIES;
-         int index2 = std::rand() % NUM_CITIES;
+         int index1 = std::rand() % cities.size();
+         int index2 = std::rand() % cities.size();
          std::swap(chromosome.path[index1], chromosome.path[index2]);
     }
 }
 
 
 // Function to generate a child chromosome from two parent chromosomes
-void generateChild(Chromosome& child, const std::vector<Chromosome> oldPopulation, const int numBestParents) {
+void generateChild(Chromosome& child, const std::vector<Chromosome> oldPopulation, const int numBestParents, const std::vector<std::vector<double>>& adjacencyMatrix) {
     // Choose 2 random parents from oldPopulation up to numBestParents
     int parentIndex1 = std::rand() % numBestParents;
     int parentIndex2 = std::rand() % numBestParents;
@@ -118,7 +136,7 @@ void generateChild(Chromosome& child, const std::vector<Chromosome> oldPopulatio
     // Perform crossover and mutation to create a new child
     crossover(child, parent1, parent2);
     mutate(child);
-    child.fitness = calculateFitness(child);
+    child.fitness = calculateFitness(child, adjacencyMatrix);
 
 }
     
@@ -128,36 +146,60 @@ void printPopulation(const std::vector<Chromosome>& oldPopulation, std::string t
     // Print all the paths next to their fitness
     for (const auto& chromosome : oldPopulation) {
         std::cout << "Path: ";
-        for (int i = 0; i < NUM_CITIES; ++i) {
+        for (int i = 0; i < cities.size(); ++i) {
             std::cout << chromosome.path[i] << " ";
         }
         std::cout << "Fitness: " << chromosome.fitness << std::endl;
     }
 }
 
-int main() {
-    // Initialize random seed
-    std::srand(std::time(nullptr));
+int main(int argc, char** argv) {
 
-    // Generate random cities. x and y coordinates are ints between 0 and MAX_COORDINATE
-    for (int i = 0; i < NUM_CITIES; ++i) {
+    // START INITIALIZATION
+
+    // std::string citiesPth = "data/fi10639.tsp";
+    // if (argc == 2 && std::string(argv[1]) == "--test") {
+    //     citiesPth = "data/wi29.tsp";
+    // }
+
+    std::string citiesPth = "data/wi29.tsp";
+
+    // Read cities from file. Each row contains the x and y coordinates of a city separated by a space.
+    std::ifstream file(citiesPth);
+    std::string line;
+    for (int i = 0; i < 7; ++i) {
+        std::getline(file, line);
+    }
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        int id;
+        double x, y;
+        if (!(iss >> id >> x >> y)) {
+            break;
+        }
         City city;
-        city.id = i;
-        city.x = std::rand() % MAX_COORDINATE;
-        city.y = std::rand() % MAX_COORDINATE;
+        city.id = id - 1;
+        city.x = x;
+        city.y = y;
         cities.push_back(city);
     }
 
-    // Generate initial old population
+    // Calculate the distance between each pair of cities and store it in an adjacency matrix
+    std::vector<std::vector<double>> adjacencyMatrix = generateAdjacencyMatrix(cities);
+
+    // Initialize random seed
+    std::srand(std::time(nullptr));
+
+    // Generate initial old population (random)
     oldPopulation.resize(POPULATION_SIZE);
     for (int i = 0; i < POPULATION_SIZE; ++i) {
-        oldPopulation[i] = generateRandomChromosome();
+        oldPopulation[i] = generateRandomChromosome(adjacencyMatrix);
     }
     // Generate initial next population (empty)
     nextPopulation.resize(POPULATION_SIZE);
     for (int i = 0; i < POPULATION_SIZE; ++i) {
         Chromosome chromosome;
-        chromosome.path.resize(NUM_CITIES);
+        chromosome.path.resize(cities.size());
         nextPopulation[i] = chromosome;
     }
 
@@ -174,7 +216,7 @@ int main() {
 
         // Iterate over each chromosome in the next population and generate a child
         for (int j = 0; j < POPULATION_SIZE; ++j) {
-            generateChild(nextPopulation[j], oldPopulation, numBestParents);
+            generateChild(nextPopulation[j], oldPopulation, numBestParents, adjacencyMatrix);
         }
 
         // Invert the populations for next iteration
@@ -190,7 +232,7 @@ int main() {
     // Print the best fitness and path at the end in the old population
     std::cout << "Best fitness at final iteration: " << oldPopulation[0].fitness << std::endl;
     std::cout << "Best path: ";
-    for (int i = 0; i < NUM_CITIES; ++i) {
+    for (int i = 0; i < cities.size(); ++i) {
         std::cout << oldPopulation[0].path[i] << " ";
     }
     std::cout << std::endl;
